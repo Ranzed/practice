4e60e12682159c2736d7d3db3713ddb43ceba959
ranzed <serega@ranzed.com>
2021-10-03 20:58:14 +0400

    [ADD] заготовки для realtimeDb

--- realtimedb/app/src/main/java/com/ranzed/realtimedb/activity/MainActivity.kt
+++ realtimedb/app/src/main/java/com/ranzed/realtimedb/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ranzed.realtimedb.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.ranzed.realtimedb.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initFragments()
+        loadDb()
+    }
+
+    private fun initFragments() {
+
+    }
+
+    private fun loadDb() {
+        val realtimeDb = Firebase.database("https://practice-adc43-default-rtdb.europe-west1.firebasedatabase.app/")
+        val ref = realtimeDb.reference
     }
 }
